22957a79780a678799308bde43145cd3a65b90f5
Stefan Sjöberg <bird.nerd@outlook.com>
2024-11-21 15:11:09 +0100

    Add project files.

---
+++ ExerciseTask01_Rewrite01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+/* Typ 50/50 på Chatgpt-kod - Kommentar från originalappen. Gjort så du kan ta bort. */
+
+List<string> ShoppingList = new List<string>();
+Console.WriteLine("Välkommen till din shoppinglista3000! \n");
+Console.WriteLine("Skriv in produkter du vill lägga till, behöver du ta bort får du göra klart listan.\n");
+
+// Loopar igenom tills avslutningsord är skrivet
+while (true)
+{
+    // Uppmanar användaren att skriva produkter till listan
+    Console.Write("Inmatning: ");
+    string ShoppingItem = Console.ReadLine()!;
+
+    // Skriver användaren "mätt" eller "klar" visas färdiga listan och sen avslutas inmatningen
+    if (ShoppingItem.ToLower() == "klar")
+    {
+        Console.Clear();
+        Console.WriteLine("Klar med inmatningen. Din slutgiltiga lista: \n");
+        break;
+    }
+    // Lägger till i listan
+    ShoppingList.Add(ShoppingItem);
+}
+
+// Skriver ut listan efter användaren har skrivit avslutningsord
+foreach (string Shopping in ShoppingList)
+{
+    Console.WriteLine("- " + Shopping);
+}
+
+// Second part of program - Remove a product
+while (true)
+{
+    // Skriv produktens namn för ta bort den
+    Console.WriteLine("Skriv in namnet på produkten som du vill ta bort. För att avsluta: skriv sluta eller nej");
+    string ShoppingItem = Console.ReadLine()!;
+
+    // Avslutningsord: sluta eller nej
+    if (ShoppingItem.ToLower() == "sluta" || ShoppingItem.ToLower() == "nej")
+    {
+        Console.Clear();
+        Console.WriteLine("Klar med inmatningen. Så ser listan ut efter ändringar: \n");
+        break;
+    }
+    // Tar bort förmålet som blev inskrivet
+    ShoppingList.Remove(ShoppingItem);
+}
+
+// Visar nya listan
+foreach (string Shopping in ShoppingList)
+{
+    Console.WriteLine("- " + Shopping);
+}
+
+Console.ReadKey();
